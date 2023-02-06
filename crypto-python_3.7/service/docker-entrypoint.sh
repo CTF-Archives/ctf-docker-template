893f422317e98ff911078a9f918c08320a564ff4
Randark <randark@foxmail.com>
2023-02-06 22:13:56 +0800

    fix error

--- crypto-python_3.7/service/docker-entrypoint.sh
+++ crypto-python_3.7/service/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-socat -v -s TCP4-LISTEN:9999,tcpwrap=script,reuseaddr,fork EXEC:"python3 /app/main.py",pty,ctty,setsid,stderr
+socat -v -s TCP4-LISTEN:9999,tcpwrap=script,reuseaddr,fork EXEC:"python3 /app/main.py"
--- crypto-sagemath/service/docker-entrypoint.sh
+++ crypto-sagemath/service/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-socat -v -s TCP4-LISTEN:9999,tcpwrap=script,reuseaddr,fork EXEC:"sage /home/sage/main.sage",pty,ctty,setsid,stderr
+socat -v -s TCP4-LISTEN:9999,tcpwrap=script,reuseaddr,fork EXEC:"sage /home/sage/main.sage"
