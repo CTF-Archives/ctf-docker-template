33f93f60f90a16da35a8e75ae7798ca590f1399d
Randark_JMT <randark@foxmail.com>
2023-07-16 02:07:04 +0000

    更新web-java-jar-openjdk8模板

---
+++ web-java-jar-openjdk8/service/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Get the user
+user=$(ls /home)
+
+# Check the environment variables for the flag and assign to INSERT_FLAG
+if [ "$DASFLAG" ]; then
+    INSERT_FLAG="$DASFLAG"
+    export DASFLAG=no_FLAG
+    DASFLAG=no_FLAG
+elif [ "$FLAG" ]; then
+    INSERT_FLAG="$FLAG"
+    export FLAG=no_FLAG
+    FLAG=no_FLAG
+elif [ "$GZCTF_FLAG" ]; then
+    INSERT_FLAG="$GZCTF_FLAG"
+    export GZCTF_FLAG=no_FLAG
+    GZCTF_FLAG=no_FLAG
+else
+    INSERT_FLAG="flag{TEST_Dynamic_FLAG}"
+fi
+
+# 将FLAG写入文件 请根据需要修改
+echo $INSERT_FLAG | tee /flag
+
+# 控制flag和项目源码的权限
+chmod 744 /flag
+chmod 740 /app/*
+
+# 运行jar程序文件
+java -jar /app/app.jar
